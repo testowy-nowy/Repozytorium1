a2614f70b1463e808dd745747b271905e9ee7132
testowy-nowy <testowyff@wp.pl>
2017-01-21 17:52:07 +0100

    Pierwszy commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	cout << "Podaj liczbe: ";
+	cin >> a;
+	cout << "\nTwoja liczba: " << a << "   Jest GIT jest Git :)";
+	return 0;
+}
